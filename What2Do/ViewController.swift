26dd9d0573d1372590c074034edff54946d900a6
Kevin Cheung <kcheung96@gmail.com>
2020-03-30 19:41:41 -0400

    Initial Commit

---
+++ What2Do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What2Do
+//
+//  Created by Kevin Cheung on 3/30/20.
+//  Copyright © 2020 Kevin Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
