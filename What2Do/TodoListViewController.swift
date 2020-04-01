3057eb01d70c0a8a57124793eccba26cfeed7f54
Kevin Cheung <kcheung96@gmail.com>
2020-03-31 23:35:05 -0400

    Add new item functionality complete

--- What2Do/TodoListViewController.swift
+++ What2Do/TodoListViewController.swift
@@ -69,7 +69,7 @@ class TodoListViewController: UITableViewController {
         }
         
         alert.addAction(action)
-        
+        //for second commit
         present(alert, animated: true, completion: nil)
     }
     
