402ad5f69e7a8311478123eb1fe808df02b253bb
Kevin Cheung <kcheung96@gmail.com>
2020-04-01 23:37:40 -0400

    Convert to MVC design pattern.

---
+++ What2Do/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  What2Do
+//
+//  Created by Kevin Cheung on 4/1/20.
+//  Copyright © 2020 Kevin Cheung. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
